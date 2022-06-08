4da23eadcfd6d70b7230daf03215a22d6c7fe0f4
brixz <diazb6904@gmail.com>
2022-06-07 22:51:34 -0300

    esta en rango

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -99,11 +99,12 @@ esDiezOCinco(10);
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
-  // Tu código:
-  if (numero <= 50 && numero >= 20) {
+  // Tu código:npm   if (numero <= 50 && numero >= 20) {
     return true;
+  }
+  return false;
 }
-
+estaEnRango(35);
 function esEntero(numero) {
   // Devuelve "true" si "numero" es un entero (int/integer)
   // Ejemplo: 0.8 -> false
