ff8ae63ad0dcd5c75412164c611db06c1efdbbe7
brixz <diazb6904@gmail.com>
2022-06-07 22:23:50 -0300

    njjj

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -18,7 +18,7 @@ function mayoriaDeEdad(edad) {
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
   if (edad >= 18) {
-    return "allowed";
+    return "Allowed";
   }
   return "Not allowed"
 }
@@ -30,7 +30,17 @@ function conection(status) {
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
+  if (status == 1){
+    return "Online";
 }
+else if(status == 2){
+    return "Away";
+}
+else {
+    return "Offline";
+}
+}
+conection(1);//Online
 
 function saludo(idioma) {
   // Devuelve un saludo en tres diferentes lenguajes:
