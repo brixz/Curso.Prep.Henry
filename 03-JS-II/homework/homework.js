a5de907ae304b5eaaedb3b98bd0452621384345d
brixz <diazb6904@gmail.com>
2022-06-08 14:31:11 -0300

    jd

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -128,7 +128,12 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
-  e
+  for (var i = 1; i < 101; i++) {
+    if (i % 15 == 0) console.log("FizzBuzz");
+    else if (i % 3 == 0) console.log("Fizz");
+    else if (i % 5 == 0) console.log("Buzz");
+    else console.log(i);
+}
 }
 
 function operadoresLogicos(num1, num2, num3) {
