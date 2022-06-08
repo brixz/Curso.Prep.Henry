e99783dec07ee634bf75ea27fb3594208bb22b72
brixz <diazb6904@gmail.com>
2022-06-07 21:11:29 -0300

    g

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -131,7 +131,7 @@ function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-  if(num % 2 === 0){
+  if(num % 2 === 1){
       return true;
   }
   else{
