761c2f5a4f9bfa8104d51e8dc7c5b0bfb8371b89
brixz <diazb6904@gmail.com>
2022-06-07 22:39:05 -0300

    colors

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -68,8 +68,24 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
+  switch (color) {
+    case "blue":
+      return "This is blue";
+      break;
+    case "red":
+      return "This is red";
+      break;
+    case "green":
+      return "This is green";
+      break;
+    case "orange":
+     return "This is orange";
+     break;
+    default:"Color not found"
+      break;
+  }
 }
-
+colors("green");
 function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
