2e4d465f6cef6a2f0abda2cd0f5117e2b2a25097
brixz <diazb6904@gmail.com>
2022-06-09 20:17:05 -0300

    ggv

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -3,8 +3,9 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un  array (pasado por parametro)
   // Tu código:
+  return array[0];
 }
-
+devolverPrimerElemento(Array[10, 34,7, 7]);
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
