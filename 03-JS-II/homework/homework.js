fd6c3bdb37f9e234e8e959e03fe6a486f4b042ab
brixz <diazb6904@gmail.com>
2022-06-12 00:08:10 -0300

    tienetresdigitos

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -211,7 +211,7 @@ tablaDelSeis(6);
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-if (numero < 99 && numero > 1000) {
+if (numero > 99 && numero < 1000) {
   return true;
 }  
 return false;
