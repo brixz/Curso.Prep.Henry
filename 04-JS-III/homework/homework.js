6df2475210ecaa6869522f7a025dda2dc176276b
brixz <diazb6904@gmail.com>
2022-06-12 23:55:59 -0300

    jj

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -47,7 +47,7 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
-  array[array.length] = array.unshift(elemento);
+  array.unshift(elemento);
   return array;
 }
 agregarItemAlComienzoDelArray("Brian", "Diaz");
