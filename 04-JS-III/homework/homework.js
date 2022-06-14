7b475e36858dd1b3e2d0937a8b390e13e3f08455
brixz <diazb6904@gmail.com>
2022-06-14 20:51:36 -0300

    includes

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -64,6 +64,8 @@ function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
+  array.includes(elemento);
+  return array;
  }
 
 function agregarNumeros(numeros) {
