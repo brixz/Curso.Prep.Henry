dd32f77d20f4a3dda3bf8f4bd4d1f14d9e29521e
brixz <diazb6904@gmail.com>
2022-06-15 19:41:14 -0300

    meow

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -9,16 +9,22 @@ function crearGato(nombre, edad) {
   var newobj ={
     nombre,
     edad,
+    meow: function () {
+      return "Meow!";
+    }
   };
 return newobj;
 }
-
 
 function agregarPropiedad(objeto, property) {
   // Agrega una propiedad al objeto (argumento "objeto") con el valor `null`
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
+  cont newobj ={
+    objeto:"null",
+
+  }
 }
 
 function invocarMetodo(objeto, metodo) {
