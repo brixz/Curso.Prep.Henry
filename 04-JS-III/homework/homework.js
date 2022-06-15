7e38c58d85af89ad14ff8b89d686708a5d4c5616
brixz <diazb6904@gmail.com>
2022-06-14 23:41:53 -0300

    h

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -105,7 +105,7 @@ function numeroMasGrande(numeros) {
   return mayor;
 }
 
-function multiplicarArgumentos(arguments) {
+function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
