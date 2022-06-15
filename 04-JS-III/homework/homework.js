ae347d295dd0d6b031cad09e12f59f4bd9e08c38
brixz <diazb6904@gmail.com>
2022-06-14 23:53:19 -0300

    t

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -121,7 +121,6 @@ function multiplicarArgumentos() {
     }
     return mult;
 }
-multiplicarArgumentos(5);
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
