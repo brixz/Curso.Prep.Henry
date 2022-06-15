e8729f1bb3abf6dcd15fd268b69d7cfd8ceb6620
brixz <diazb6904@gmail.com>
2022-06-15 14:16:33 -0300

    cuentoElemento

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -121,6 +121,12 @@ function multiplicarArgumentos() {
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
+  var registrar = [];
+  for(let i = 0; i < arreglo.length; i++){
+    if(arreglo[i] < 18){
+      registrar++;
+    }
+  }
 }
 
 
