1c432571a1523f5e5e8bb62a8e6604ebdbee1088
brixz <diazb6904@gmail.com>
2022-06-16 19:32:48 -0300

    this.precio

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -142,8 +142,8 @@ function agregarMetodoCalculoDescuento(producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
-    producto["calcularPrecioDescuento"]= function() {
-       this.precio - (this.calcularPrecioDescuento * this.porcentajeDeDescuento) ;
+    producto[calcularPrecioDescuento]= function() {
+       this.precio - (this.precio * this.porcentajeDeDescuento) ;
     }
     return producto;
 }
