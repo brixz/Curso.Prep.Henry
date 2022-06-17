990a64e0d73065b5bed0288d4e7c6fe4ab30a64f
brixz <diazb6904@gmail.com>
2022-06-17 00:10:50 -0300

    dato

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -79,9 +79,9 @@ function agregarMetodo() {
   //La función agrega un método "datos" a la clase Persona que toma el nombre y la edad de la persona y devuelve: 
   //Ej: "Juan, 22 años"
   Persona.prototype.dato =function() {
-    dato = this.nombre +", " + this.edad +" años";
+   return dato = this.nombre +", " + this.edad +" años";
   }
-  return Persona;
+
 }
   
 
