b9a0c940d06b5ff0439dd6e1e3c662823dd11673
brixz <diazb6904@gmail.com>
2022-06-17 21:28:46 -0300

    forEach

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -37,9 +37,11 @@ function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
-array.forEach(cb);
+ for(var i = 0; i < array.length; i++){
+  cb(array[i]);
 }
-
+}
+// array.forEach(cb);
 function map(array, cb) {
   // Crea un nuevo array
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
