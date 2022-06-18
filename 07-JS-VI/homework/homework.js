adefaa5fb837cc5874de18d264a46b8a37b575db
brixz <diazb6904@gmail.com>
2022-06-17 21:04:15 -0300

    sumadeArray

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -25,12 +25,12 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
-  var resultado =[];
+  var resultado =[0];
   for (let i = 0; i < numeros.length; i++) {
     resultado = resultado[i] + numeros[i];
     
   }
-  cb(resultado)
+  cb(resultado);
 }
 
 function forEach(array, cb) {
